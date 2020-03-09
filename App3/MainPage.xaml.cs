44cc997cb216a625a6411bb3acf2ffd5be792f64
Jaime Correa <jaimec@rts.com.co>
2020-03-09 08:28:07 -0500

    Se están mostrando los botones para posicionarlos sobre cada jugador a nivel de interfaz. Se presenta pantalla negra entre la transición de la pantalla de jugadores y escoger jugador

--- App3/MainPage.xaml.cs
+++ App3/MainPage.xaml.cs
@@ -29,13 +29,15 @@ namespace App3
             Fondo.Play();
         }
 
-        void IraVerJugadores(object sender, RoutedEventArgs e)
+        void verJugadores(object sender, RoutedEventArgs e)
         {
             Fondo2.Visibility = Visibility.Visible;
             Fondo.Source = new Uri(this.BaseUri, "/Assets/LoopJugadores.mp4");
             Fondo.Visibility = Visibility.Collapsed;
             Fondo2.IsLooping = false;
             Fondo2.Play();
+            btnVoyPaEsa.Visibility = Visibility.Collapsed;
+            Fondo2.MediaEnded += Finvideo2;
         }
 
         void Finvideo2(object sender, RoutedEventArgs e)
@@ -45,8 +47,90 @@ namespace App3
             
             Fondo.IsLooping = true;
             Fondo.Play();
+
+            // Mostrar los botones de los jugadores
+            btnTeofilo.Visibility = Visibility.Visible;
+            btnFredy.Visibility = Visibility.Visible;
+            btnMiguel.Visibility = Visibility.Visible;
+            btnSebastian.Visibility = Visibility.Visible;
+            btnMarlon.Visibility = Visibility.Visible;
+            btnDidier.Visibility = Visibility.Visible;
+            btnTodos.Visibility = Visibility.Visible;
         }
 
+        void FinTeo(object sender, RoutedEventArgs e)
+        {
+            Fondo.Visibility = Visibility.Collapsed;
+            btnCapturar.Visibility = Visibility.Visible;
+            Fondo2.Visibility = Visibility.Visible;
+            Fondo2.IsLooping = true;
+            Fondo2.Play();
+        }
 
+        void seleccionarJugador(object sender, RoutedEventArgs e)
+        {
+            Button boton = sender as Button;
+            String opcionSeleccionada = "";
+            switch (boton.Name)
+            {
+                case "btnTeofilo":
+                    opcionSeleccionada = "Teófilo Gutiérrez";
+                    Fondo.Source = new Uri(this.BaseUri, "/Assets/Teo_seleccion.mp4");
+                    Fondo2.Source = new Uri(this.BaseUri, "/Assets/TeoLoop.mp4");
+                    Fondo.Visibility = Visibility.Visible;
+                    Fondo2.Visibility = Visibility.Collapsed;
+                    Fondo.IsLooping = false;
+                    Fondo.Play();
+                    Fondo.MediaEnded += FinTeo;
+                    break;
+
+                case "btnFredy":
+                    opcionSeleccionada = "Fredy Hinestroza";
+                    break;
+
+                case "btnMiguel":
+                    opcionSeleccionada = "Miguel Borja";
+                    break;
+
+                case "btnSebastian":
+                    opcionSeleccionada = "Sebastián Viera";
+                    break;
+
+                case "btnMarlon":
+                    opcionSeleccionada = "Marlon Piedrahita";
+                    break;
+
+                case "btnDidier":
+                    opcionSeleccionada = "Didier Moreno";
+                    break;
+
+                case "btnTodos":
+                    opcionSeleccionada = "Todos los jugadores";
+                    break;
+
+                default:
+                    break;
+            }
+
+            ContentDialog contentDialog = new ContentDialog
+            {
+                Title = "Opción seleccionada",
+                Content = opcionSeleccionada,
+                CloseButtonText = "Cerrar"
+            };
+
+            contentDialog.ShowAsync();
+        }
+        void capturar(object sender, RoutedEventArgs e)
+        {
+            ContentDialog contentDialog = new ContentDialog
+            {
+                Title = "Capturar",
+                Content = "Foto",
+                CloseButtonText = "Cerrar"
+            };
+
+            contentDialog.ShowAsync();
+        }
     }
 }
